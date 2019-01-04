ba2a29c90911835cc16b35e999178dcec8b4ea14
PeterTing <a0917209079@gmail.com>
2019-01-04 17:21:14 +0800

    fixing

--- app/router/serviceEvent.js
+++ app/router/serviceEvent.js
@@ -1,12 +1,4 @@
 ﻿"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
@@ -21,19 +13,12 @@ const richMenuScript_1 = require("../lib/richMenuScript");
 const router = express.Router();
 exports.serviceEvent = router;
 const logFactory = require('../lib/logFactory')('linebot:webhook/serviceEvent');
-router.post('/', (req, res) => __awaiter(this, void 0, void 0, function* () {
+router.post('/', (req, res) => {
     res.status(200);
-    try {
-        let result = yield tool_1.getUserDetail(req.body.para);
-        if (result) {
-            return richMenuScript_1.switchRichmenu(result.usingAmount + result.lostAmount, result.lineToken);
-        }
-    }
-    catch (err) {
-        logFactory.error(err);
-        res.status(404);
-    }
-}));
+    tool_1.getUserDetail(req.body.para)
+        .then(result => richMenuScript_1.switchRichmenu(result.usingAmount + result.lostAmount, result.lineToken))
+        .catch(err => logFactory.error("err"));
+});
 function getip(req) {
     return req.headers["x-real-ip"] ||
         req.ip ||
--- app_ts/router/serviceEvent.ts
+++ app_ts/router/serviceEvent.ts
@@ -4,18 +4,12 @@ import { switchRichmenu } from '../lib/richMenuScript';
 const router = express.Router();
 const logFactory = require('../lib/logFactory')('linebot:webhook/serviceEvent');
 
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
     res.status(200);
 
-    try {
-        let result = await getUserDetail(req.body.para);
-        if (result) {
-            return switchRichmenu(result.usingAmount + result.lostAmount, result.lineToken);
-        }
-    } catch (err) {
-        logFactory.error(err);
-        res.status(404);
-    }
+    getUserDetail(req.body.para)
+        .then(result => switchRichmenu(result.usingAmount + result.lostAmount, result.lineToken))
+        .catch(err => logFactory.error("err"));
 });
 
 
